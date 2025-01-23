050378c19fa15591a18bb5f158165d38ccb3a4ac
sushilm20 <sushil.m@icloud.com>
2025-01-23 16:53:24 -0500

    The Big Bad Wolf joined...

--- TeamCode/src/main/java/org/riverdell/robotics/autonomous/impl/intothedeep/Sushil.kt
+++ TeamCode/src/main/java/org/riverdell/robotics/autonomous/impl/intothedeep/Sushil.kt
@@ -12,11 +12,22 @@ import org.riverdell.robotics.subsystems.outtake.OuttakeLevel
 @Autonomous(name = "hors")
 class Sushil : HypnoticAuto({ robot ->
     single("hors") {
-        robot.robot.intakeComposite.outtakeLevel(OuttakeLevel.Bar2)
-        navigateTo(Pose(-1.5, -15.69, 46.degrees))
-        robot.robot.outtake.setClawState(ClawState.Open)
 
-        navigateTo(Pose(0.0, 0.0, 0.degrees))
-        robot.robot.intakeComposite.outtakeLevel(OuttakeLevel.Rest)
+        navigateTo(Pose(0.0, -28.4, 0.degrees))
+//        robot.robot.intakeComposite.outtakeLevel(OuttakeLevel.Bar2)
+//        (OuttakeLevel.Bar2)
+//        robot.robot.outtake.setClawState(ClawState.Open)
+
+        navigateTo(Pose(-36.6, -28.8, 0.degrees))
+//        robot.robot.intakeComposite.outtakeLevel(OuttakeLevel.Rest)
+        navigateTo(Pose(-38.0, -49.0, 0.degrees))
+        navigateTo(Pose(-44.0, -51.65, 0.degrees))
+        navigateTo(Pose(-45.6, -15.72, 0.degrees))
+        navigateTo(Pose(-44.0, -51.65, 0.degrees))
+        navigateTo(Pose(-54.37, -51.0, 0.degrees))
+        navigateTo(Pose(-55.8, -17.5, 0.degrees))
+        navigateTo(Pose(-56.9, -56.2, 0.degrees))
+        navigateTo(Pose(-64.3, -66.1, 0.degrees))//change
+        navigateTo(Pose(-63.3, -22.4, 0.degrees))
     }
 })
