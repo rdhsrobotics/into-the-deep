d4343a6e4aebb0aeee71a951b8e87d3ae20968b5
sushilm20 <sushil.m@icloud.com>
2025-01-27 19:04:31 -0500

    The Big Bad Wolf joined...

--- TeamCode/src/main/java/org/riverdell/robotics/autonomous/impl/intothedeep/Sushil.kt
+++ TeamCode/src/main/java/org/riverdell/robotics/autonomous/impl/intothedeep/Sushil.kt
@@ -5,29 +5,35 @@ import io.liftgate.robotics.mono.pipeline.single
 import org.riverdell.robotics.autonomous.HypnoticAuto
 import org.riverdell.robotics.autonomous.movement.degrees
 import org.riverdell.robotics.autonomous.movement.geometry.Pose
-import org.riverdell.robotics.autonomous.movement.navigateTo
+import org.riverdell.robotics.autonomous.movement.purePursuitNavigateTo
 import org.riverdell.robotics.subsystems.outtake.ClawState
 import org.riverdell.robotics.subsystems.outtake.OuttakeLevel
+import org.riverdell.robotics.autonomous.movement.purepursuit.FieldWaypoint
 
 @Autonomous(name = "hors")
 class Sushil : HypnoticAuto({ robot ->
     single("hors") {
 
-        navigateTo(Pose(0.0, -28.4, 0.degrees))
+        purePursuitNavigateTo(
+            FieldWaypoint(Pose(0.0, -28.4, 0.degrees), 10.0),
+            FieldWaypoint(Pose(-21.49, -10.27,270.degrees),10.0),
+            FieldWaypoint(Pose(-36.6, -10.9, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-37.24, -33.0, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-45.6, -39.8, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-46.00, -3.23, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-45.6, -37.43, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-54.9, -37.04, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-54.33, -2.24, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-56.5, -38.26, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-61.81, -34.63, 180.degrees), 10.0),
+            FieldWaypoint(Pose(-53.00, -4.14, 180.degrees), 10.0), //change
+            FieldWaypoint(Pose(-31.92, 6.33, 180.degrees), 10.0)
+        ) {
+            disableAutomaticDeath()
+        }
 //        robot.robot.intakeComposite.outtakeLevel(OuttakeLevel.Bar2)
 //        (OuttakeLevel.Bar2)
 //        robot.robot.outtake.setClawState(ClawState.Open)
-
-        navigateTo(Pose(-36.6, -28.8, 0.degrees))
 //        robot.robot.intakeComposite.outtakeLevel(OuttakeLevel.Rest)
-        navigateTo(Pose(-38.0, -49.0, 0.degrees))
-        navigateTo(Pose(-44.0, -51.65, 0.degrees))
-        navigateTo(Pose(-45.6, -15.72, 0.degrees))
-        navigateTo(Pose(-44.0, -51.65, 0.degrees))
-        navigateTo(Pose(-54.37, -51.0, 0.degrees))
-        navigateTo(Pose(-55.8, -17.5, 0.degrees))
-        navigateTo(Pose(-56.9, -56.2, 0.degrees))
-        navigateTo(Pose(-64.3, -66.1, 0.degrees))//change
-        navigateTo(Pose(-63.3, -22.4, 0.degrees))
     }
 })
---
+++ TeamCode/src/main/java/org/riverdell/robotics/autonomous/impl/intothedeep/SushilOld.kt
@@ -0,0 +1,33 @@
+package org.riverdell.robotics.autonomous.impl.intothedeep
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import io.liftgate.robotics.mono.pipeline.single
+import org.riverdell.robotics.autonomous.HypnoticAuto
+import org.riverdell.robotics.autonomous.movement.degrees
+import org.riverdell.robotics.autonomous.movement.geometry.Pose
+import org.riverdell.robotics.autonomous.movement.navigateTo
+import org.riverdell.robotics.subsystems.outtake.ClawState
+import org.riverdell.robotics.subsystems.outtake.OuttakeLevel
+
+@Autonomous(name = "Specimen + Park")
+class SushilOld : HypnoticAuto({ robot ->
+    single("hors") {
+
+        navigateTo(Pose(0.0, -28.4, 0.degrees))
+//        robot.robot.intakeComposite.outtakeLevel(OuttakeLevel.Bar2)
+//        (OuttakeLevel.Bar2)
+//        robot.robot.outtake.setClawState(ClawState.Open)
+
+        navigateTo(Pose(-36.6, -28.8, 0.degrees))
+//        robot.robot.intakeComposite.outtakeLevel(OuttakeLevel.Rest)
+        navigateTo(Pose(-38.0, -49.0, 0.degrees))
+        navigateTo(Pose(-44.0, -51.65, 0.degrees))
+        navigateTo(Pose(-45.6, -15.72, 0.degrees))
+        navigateTo(Pose(-44.0, -51.65, 0.degrees))
+        navigateTo(Pose(-54.37, -51.0, 0.degrees))
+        navigateTo(Pose(-55.8, -17.5, 0.degrees))
+        navigateTo(Pose(-56.9, -56.2, 0.degrees))
+        navigateTo(Pose(-64.3, -66.1, 0.degrees))//change
+        navigateTo(Pose(-63.3, -22.4, 0.degrees))
+    }
+})
